d08ca681afa7e28215e6bb97afa330a41746928e
Michael E. Ershov <em.ershov@yandex.ru>
2025-11-12 16:19:49 +0800

    add load config

---
+++ internal/auth/handler.go
@@ -0,0 +1,38 @@
+package auth
+
+import (
+	"fmt"
+	"go/adv-demo/configs"
+	"net/http"
+)
+
+type AuthHandlerDeps struct{
+	Config *configs.Config
+}
+
+type AuthHandler struct{
+	Config *configs.Config
+}
+
+func AuthHandlerConstructor(router *http.ServeMux, deps AuthHandlerDeps) {
+	handler := &AuthHandler{
+		Config: deps.Config,
+	}
+	router.HandleFunc("/auth/login", handler.Login())
+	router.HandleFunc("/auth/register", handler.Register())
+}
+
+func (handler *AuthHandler) Login() http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		fmt.Println(handler.Config.Auth.Secret)
+		fmt.Println("login")
+		return
+	}
+}
+
+func (handler *AuthHandler) Register() http.HandlerFunc {
+	return func(w http.ResponseWriter, req *http.Request) {
+		fmt.Println("register")
+		return
+	}
+}
