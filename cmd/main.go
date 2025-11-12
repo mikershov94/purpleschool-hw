9bef7b228541628850aad5a351eac311bf2cf6d6
Michael E. Ershov <em.ershov@yandex.ru>
2025-11-12 16:19:49 +0800

    add load config

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"fmt"
+	"go/adv-demo/configs"
+	"go/adv-demo/internal/auth"
 	"net/http"
 )
 
 func main() {
+	conf := configs.LoadConfig()
 	router := http.NewServeMux()
-	RandomHandlerConstructor(router)
+	auth.AuthHandlerConstructor(router, auth.AuthHandlerDeps{
+		Config: conf,
+	})
 
 	server := http.Server{
 		Addr:    ":5000",
