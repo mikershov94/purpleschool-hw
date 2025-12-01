2faa15f4d9bcb11ea0ccb1494b105b9f9db9b7b3
Michael E. Ershov <em@newton.finance>
2025-12-01 16:59:10 +0800

    added port to configs

--- cmd/main.go
+++ cmd/main.go
@@ -2,18 +2,27 @@ package main
 
 import (
 	"fmt"
+	"go/validation-api/configs"
 	"net/http"
 )
 
 func main() {
+	conf := configs.LoadConfig()
+
 	router := http.NewServeMux()
 
+	port := conf.Server.Port
 	server := http.Server{
-		Addr:    ":3000",
+		Addr:    fmt.Sprintf(":%d", port),
 		Handler: router,
 	}
 
-	fmt.Println("Server starting on port 3000")
+	if port == 0 {
+		fmt.Println("Server failed: uncorrect port")
+		return
+	}
+	fmt.Println("Server starting on port", port)
+
 	if err := server.ListenAndServe(); err != nil {
 		fmt.Printf("Server failed: %v\r\n", err)
 		return
