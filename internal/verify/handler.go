c8c8d54f1d8a8b2d1667ffcf4912de8590e8fbc1
Michael E. Ershov <em.ershov@yandex.ru>
2026-01-14 12:53:09 +0800

    added sending letter

--- internal/verify/handler.go
+++ internal/verify/handler.go
@@ -1,6 +1,8 @@
 package verify
 
 import (
+	"crypto/sha256"
+	"fmt"
 	"go/validation-api/configs"
 	"net/http"
 	"net/smtp"
@@ -25,19 +27,31 @@ func (handler *VerifyHandler) Send() http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(201)
 
+		hash := sha256.Sum256([]byte(handler.Config.Email))
+		// fmt.Println()
+		// TODO
+		// хэш перевести в строку и вставить в HTML
+
 		e := email.NewEmail()
-		e.From = "Michael Ershov"
-		e.To = []string{handler.Config.Email}
+		e.From = "<" + handler.Config.Email + ">"
+		e.To = []string{"<" + handler.Config.Email + ">"}
 		e.Subject = "Michael Ershov"
 		e.Text = []byte("Text Body is, of course, supported!")
-		e.HTML = []byte("<h1>Fancy HTML is supported, too!</h1>")
-		e.Send("smtp.gmail.com:587", smtp.PlainAuth("", handler.Config.Email, handler.Config.Password, handler.Config.Address))
+		e.HTML = []byte(fmt.Sprintf(`<a href="http://localhost:3000/verify/%x">http://localhost:3000/verify/</a>`, hash))
+		err := e.Send(handler.Config.Address + ":587", smtp.PlainAuth("", handler.Config.Email, handler.Config.Password, handler.Config.Address))
+		if err != nil {
+			fmt.Println("Не удалось отправить письмо")
+			fmt.Println(err)
+			return
+		}
 
+		fmt.Println("Письмо отправлено")
 	}
 }
 
 func (handler *VerifyHandler) Verify() http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		w.WriteHeader(200)
+		fmt.Println("Перешел по ссылке")
 	}
 }
