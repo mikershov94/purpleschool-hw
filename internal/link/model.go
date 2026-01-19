d9e76025cc5846eda3e3fdcaba13fafc08e1406b
Michael E. Ershov <em.ershov@yandex.ru>
2026-01-19 08:44:02 +0800

    added link model

---
+++ internal/link/model.go
@@ -0,0 +1,31 @@
+package link
+
+import (
+	"math/rand"
+
+	"gorm.io/gorm"
+)
+
+type Link struct {
+	gorm.Model
+	Url  string `json:"url"`
+	Hash string `json:"hash" gorm:"uniqueIndex"`
+}
+
+func LinkConstructor(url string) *Link {
+	return &Link{
+		Url: url,
+		Hash: "",
+	}
+}
+
+var letterRunes = []rune("abcdefghijklmnoprstuvwxyzABCDEFGHIJKLMNOPRSTUVWXYZ")
+
+func RandStringRunes(n int) string {
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letterRunes[rand.Intn(len(letterRunes))]
+	}
+
+	return string(b)
+}
